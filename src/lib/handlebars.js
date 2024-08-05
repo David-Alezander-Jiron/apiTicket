02069cb6a2a99f4452ce1ed8764a61fbe9c27817
Alexander <90058418+David-Alezander-Jiron@users.noreply.github.com>
2024-08-04 21:26:59 -0500

    Delete src directory

--- src/lib/handlebars.js
+++
@@ -1,9 +0,0 @@
-const timeago = require('timeago.js');
-
-const helpers = {};
-
-helpers.formatTimeAgo = (savedTimestamp) => {
-    return timeago.format(savedTimestamp);
-};
-
-module.exports = helpers;
