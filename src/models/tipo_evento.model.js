63eba3cb7d3f7d31b1b6d3157f106413a30604c9
Alexander <90058418+David-Alezander-Jiron@users.noreply.github.com>
2024-08-06 18:12:49 -0500

    tipo_evento.model.js

--- src/models/tipo_evento.model.js
+++ src/models/tipo_evento.model.js
@@ -1,36 +1,21 @@
-const tipoEventos = (sequelize, type) => {
-    return sequelize.define('tipoEventos', {
+const tipo_eventos = (sequelize, type) => {
+    return sequelize.define('tipo_eventos', {
         id: {
             type: type.INTEGER,
             autoIncrement: true,
             primaryKey: true,
             comment: 'ID del tipo de evento'
-        },
-        tipo_id: {
-          type: type.INTEGER,
-          allowNull: false,
-          references: {
-              model: 'tipo',
-              key: 'id'
           },
-          comment: 'ID del tipo relacionado'
-      },
-      evento_id: {
-        type: type.INTEGER,
-        allowNull: false,
-        references: {
-            model: 'evento',
-            key: 'id'
-        },
-        comment: 'ID del evento relacionado'
-    },
-       
-        
-    }, {
-        timestamps: false,
-        comment: 'Tabla de tipos de eventos'
-    });
-  };
+          tipo_id: {
+            type: type.INTEGER,
+            allowNull: false,
+            comment: 'ID del tipo relacionado'
+          }
+        }, {
+          timestamps: false,
+          comment: 'Tabla de tipos de eventos'
+        });
+    }
   
-  module.exports = tipoEventos;
+  module.exports = tipo_eventos;
   
