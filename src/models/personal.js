4fd84efc8c24887ab28f2f17ec463b50a7410e16
Alexander <90058418+David-Alezander-Jiron@users.noreply.github.com>
2024-08-07 20:39:58 -0500

    personal.js

--- src/models/personal.js
+++ src/models/personal.js
@@ -1,41 +1,35 @@
 const personals = (sequelize, type) => {
   return sequelize.define('personals', {
-      id: {
-          type: type.INTEGER,
-          autoIncrement: true,
-          primaryKey: true,
-          comment: 'ID del personal'
+    id: {
+        type: type.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        comment: 'ID del personal'
       },
       nombre: {
-          type: type.STRING(50),
-          allowNull: false,
-          comment: 'Nombre del personal'
+        type: type.STRING,
+        allowNull: false,
+        comment: 'Nombre del personal'
       },
       apellido: {
-          type: type.STRING(50),
-          allowNull: false,
-          comment: 'Apellido del personal'
-      },
-      rol: {
-          type: type.STRING(50),
-          allowNull: true,
-          comment: 'Rol del personal'
+        type: type.STRING,
+        allowNull: false,
+        comment: 'Apellido del personal'
       },
       telefono: {
-          type: type.STRING(20),
-          allowNull: true,
-          comment: 'Teléfono del personal'
+        type: type.STRING,
+        allowNull: false,
+        comment: 'Teléfono del personal'
       },
-    estado: {
-            type: type.ENUM('activo','inactivo', 'eliminado'),
-            allowNull: false,
-            defaultValue: 'activo',
-            comment: 'Estado del usuario'
-        },
-  }, {
-      timestamps: false,
+      rol: {
+        type: type.STRING,
+        allowNull: false,
+        comment: 'Rol del personal'
+      }
+    }, {
+      timestamps: true,
       comment: 'Tabla de personal'
-  });
-};
+    });
+  };
 
 module.exports = personals;
